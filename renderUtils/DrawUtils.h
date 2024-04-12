953089afceacda192c36140c4f6e1c0ce27f5410
oharaAoi <k023g0108@g.neec.ac.jp>
2024-04-12 11:58:17 +0900

    パスの開通

--- renderUtils/DrawUtils.h
+++ renderUtils/DrawUtils.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 #include <Novice.h>
 #include "MyMatrix4x4.h"
 #include <cmath>
@@ -14,4 +14,11 @@ void DrawGrid(const Matrix4x4& viewPrijectionMatrix, const Matrix4x4& viewMatrix
 
 void DrawSphere(const Sphere& sphere, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix, uint32_t color);
 
+// 表示
+void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) {
+	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
+	Novice::ScreenPrintf(x + kColWidth, y, "%.02f", vector.y);
+	Novice::ScreenPrintf(x + kColWidth * 2, y, "%.02f", vector.z);
+	Novice::ScreenPrintf(x + kColWidth * 3, y, "%s", label);
+}
 
