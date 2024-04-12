49530ea0a3040846e64c9c786d74fde16ce939b0
oharaAoi <k023g0108@g.neec.ac.jp>
2024-04-12 12:26:59 +0900

    書式の変更

--- renderUtils/DrawUtils.cpp
+++ renderUtils/DrawUtils.cpp
@@ -176,3 +176,11 @@ void DrawSphere(const Sphere& sphere, const Matrix4x4& viewProjectionMatrix, con
 		}
 	}
 }
+
+// 表示
+void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label) {
+	Novice::ScreenPrintf(x, y, "%.02f", vector.x);
+	Novice::ScreenPrintf(x + kColWidth, y, "%.02f", vector.y);
+	Novice::ScreenPrintf(x + kColWidth * 2, y, "%.02f", vector.z);
+	Novice::ScreenPrintf(x + kColWidth * 3, y, "%s", label);
+}
