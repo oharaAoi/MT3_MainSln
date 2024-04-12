ccc9d4a7b079bd792ec7daf1abc37f8c6cee9d40
oharaAoi <k023g0108@g.neec.ac.jp>
2024-04-12 12:23:47 +0900

    00_02_確認課題

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <Novice.h>
+#include "Matrix4x4.h"
+#include "MyMath.h"
+#include "DrawUtils.h"
 
 const char kWindowTitle[] = "LE2A_06_オオハラアオイ";
 
@@ -13,6 +16,29 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
+
+	Matrix4x4 m1 = {
+		3.2f, 0.7f, 9.6f, 4.4f,
+		5.5f, 1.3f, 7.8f, 2.1f,
+		6.9f, 8.0f, 2.6f, 1.0f,
+		0.5f, 7.2f, 5.1f, 3.3f
+	};
+
+	Matrix4x4 m2 = {
+		4.1f, 6.5f, 3.3f, 2.2f,
+		8.8f, 0.6f, 9.9f, 7.7f,
+		1.1f, 5.5f, 6.6f, 0.0f,
+		3.3f, 9.9f, 8.8f, 2.2f
+	};
+
+	Matrix4x4 resultAdd = Add(m1, m2);
+	Matrix4x4 resultMultiply = Multiply(m1, m2);
+	Matrix4x4 resultSubtract = Subtract(m1, m2);
+	Matrix4x4 resultInverse1 = Inverse(m1);
+	Matrix4x4 resultInverse2 = Inverse(m2);
+	Matrix4x4 resultTranspose1 = Transpose(m1);
+	Matrix4x4 resultTranspose2 = Transpose(m2);
+	Matrix4x4 resultIndtity = MakeIdentity4x4();
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -38,7 +64,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓描画処理ここから
 		///------------------///
 
-
+		MatrixScreenPrintf(0, 0, resultAdd, "Add");
+		MatrixScreenPrintf(0, kRowHeight * 5, resultSubtract, "Subtract");
+		MatrixScreenPrintf(0, kRowHeight * 5 * 2, resultMultiply, "Multiply");
+		MatrixScreenPrintf(0, kRowHeight * 5 * 3, resultInverse1, "Inverse1");
+		MatrixScreenPrintf(0, kRowHeight * 5 * 4, resultInverse2, "Inverse2");
+		MatrixScreenPrintf(kColWidth * 5, 0, resultTranspose1, "Transpose1");
+		MatrixScreenPrintf(kColWidth * 5, kRowHeight * 5, resultTranspose2, "Transpose2");
+		MatrixScreenPrintf(kColWidth * 5, kRowHeight * 5 * 2, resultIndtity, "Indtity");
 
 
 		///------------------///
