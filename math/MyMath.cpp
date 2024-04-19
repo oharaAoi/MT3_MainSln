67befa63f396f8ce99a1ae84f7860f1d6ca868df
oharaAoi <k023g0108@g.neec.ac.jp>
2024-04-19 10:17:42 +0900

    数学関数の追加

--- math/MyMath.cpp
+++ math/MyMath.cpp
@@ -1,10 +1,14 @@
 ﻿#include "MyMath.h"
 
-float Length(const Vec3f& vec3){
-    return std::sqrt(vec3.x * vec3.x + vec3.y * vec3.y + vec3.z * vec3.z);
-}
-
-// 加算
+//=================================================================================================================
+//	↓　四則演算
+//=================================================================================================================
+/// <summary>
+/// 加算
+/// </summary>
+/// <param name="v1"></param>
+/// <param name="v2"></param>
+/// <returns></returns>
 Vec3f Add(const Vec3f& v1, const Vec3f& v2) {
 	Vec3f result{};
 
@@ -15,7 +19,12 @@ Vec3f Add(const Vec3f& v1, const Vec3f& v2) {
 	return result;
 }
 
-// 減算
+/// <summary>
+/// 減算
+/// </summary>
+/// <param name="v1"></param>
+/// <param name="v2"></param>
+/// <returns></returns>
 Vec3f  Subtract(const Vec3f& v1, const Vec3f& v2) {
 	Vec3f result{};
 
@@ -26,7 +35,12 @@ Vec3f  Subtract(const Vec3f& v1, const Vec3f& v2) {
 	return result;
 }
 
-// スカラー倍
+/// <summary>
+/// 掛け算
+/// </summary>
+/// <param name="scalar"></param>
+/// <param name="v"></param>
+/// <returns></returns>
 Vec3f Multiply(float scalar, const Vec3f& v) {
 	Vec3f result{};
 
@@ -37,7 +51,15 @@ Vec3f Multiply(float scalar, const Vec3f& v) {
 	return result;
 }
 
-// 内積
+//=================================================================================================================
+//	↓	ベクトルの内積外積
+//=================================================================================================================
+/// <summary>
+/// 内積
+/// </summary>
+/// <param name="v1"></param>
+/// <param name="v2"></param>
+/// <returns></returns>
 float Dot(const Vec3f& v1, const Vec3f& v2) {
 	float result{};
 
@@ -46,6 +68,39 @@ float Dot(const Vec3f& v1, const Vec3f& v2) {
 	return result;
 }
 
+/// <summary>
+/// クロス積
+/// </summary>
+/// <param name="v1"></param>
+/// <param name="v2"></param>
+/// <returns></returns>
+Vec3f Cross(const Vec3f& v1, const Vec3f& v2) {
+	Vec3f result{};
+
+	result.x = v1.y * v2.z - v1.z * v2.y;
+	result.y = v1.z * v2.x - v1.x * v2.z;
+	result.z = v1.x * v2.y - v1.y * v2.x;
+
+	return result;
+}
+
+//=================================================================================================================
+//	↓	ベクトルの長さに関する関数
+//=================================================================================================================
+/// <summary>
+/// ノルム
+/// </summary>
+/// <param name="vec3"></param>
+/// <returns></returns>
+float Length(const Vec3f& vec3) {
+	return std::sqrt(vec3.x * vec3.x + vec3.y * vec3.y + vec3.z * vec3.z);
+}
+
+/// <summary>
+/// 正規化
+/// </summary>
+/// <param name="vec3"></param>
+/// <returns></returns>
 Vec3f Normalize(const Vec3f& vec3){
     Vec3f result = vec3;
 
--- math/MyMath.h
+++ math/MyMath.h
@@ -7,14 +7,65 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 
+//=================================================================================================================
+//	↓　四則演算
+//=================================================================================================================
+/// <summary>
+/// 加算
+/// </summary>
+/// <param name="v1"></param>
+/// <param name="v2"></param>
+/// <returns></returns>
 Vec3f Add(const Vec3f& v1, const Vec3f& v2);
 
+/// <summary>
+/// 減算
+/// </summary>
+/// <param name="v1"></param>
+/// <param name="v2"></param>
+/// <returns></returns>
 Vec3f  Subtract(const Vec3f& v1, const Vec3f& v2);
 
+/// <summary>
+/// 掛け算
+/// </summary>
+/// <param name="scalar"></param>
+/// <param name="v"></param>
+/// <returns></returns>
 Vec3f Multiply(float scalar, const Vec3f& v);
 
+//=================================================================================================================
+//	↓	ベクトルの内積外積
+//=================================================================================================================
+/// <summary>
+/// 内積
+/// </summary>
+/// <param name="v1"></param>
+/// <param name="v2"></param>
+/// <returns></returns>
 float Dot(const Vec3f& v1, const Vec3f& v2);
 
+/// <summary>
+/// クロス積
+/// </summary>
+/// <param name="v1"></param>
+/// <param name="v2"></param>
+/// <returns></returns>
+Vec3f Cross(const Vec3f& v1, const Vec3f& v2);
+
+//=================================================================================================================
+//	↓	ベクトルの長さに関する関数
+//=================================================================================================================
+/// <summary>
+/// ノルム
+/// </summary>
+/// <param name="vec3"></param>
+/// <returns></returns>
 float Length(const Vec3f& vec3);
 
+/// <summary>
+/// 正規化
+/// </summary>
+/// <param name="vec3"></param>
+/// <returns></returns>
 Vec3f Normalize(const Vec3f& vec3);
