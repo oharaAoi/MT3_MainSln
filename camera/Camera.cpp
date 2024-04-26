0dc99a67729b9851f4c17189c8df7e875ed20559
oharaAoi <k023g0108@g.neec.ac.jp>
2024-04-26 09:56:02 +0900

    カメラの準備

--- camera/Camera.cpp
+++ camera/Camera.cpp
@@ -1,16 +1,16 @@
-#include "Camera.h"
+﻿#include "Camera.h"
 
 Camera::Camera() { Init(); }
 
 void Camera::Init(){
 
-	pos_ = { 0.0f, 1.9f, -6.49f };
+	translation_ = { 0.0f, 1.9f, -6.49f };
 	rotate_ = { 0.26f, 0.0f, 0.0f };
 
-	cameraMatrix_ = MakeAffineMatrix({ 1.0f, 1.0f, 1.0f }, rotate_, pos_);
-
+	cameraMatrix_ = MakeAffineMatrix({ 1.0f, 1.0f, 1.0f }, rotate_, translation_);
+	// ビュー行列
 	viewMatrix_ = Inverse(cameraMatrix_);
-
+	// 射影行列
 	projectionMatrix_ = MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
 
 	viewProjectMatrix_ = Multiply(viewMatrix_, projectionMatrix_);
@@ -21,7 +21,7 @@ void Camera::Init(){
 
 void Camera::Update(){
 
-	cameraMatrix_ = MakeAffineMatrix({ 1.0f, 1.0f, 1.0f }, rotate_, pos_);
+	cameraMatrix_ = MakeAffineMatrix({ 1.0f, 1.0f, 1.0f }, rotate_, translation_);
 
 	viewMatrix_ = Inverse(cameraMatrix_);
 
@@ -35,8 +35,19 @@ void Camera::Update(){
 void Camera::Draw(){
 	ImGui::Begin("window");
 
-	ImGui::DragFloat3("cameraTranslate", &pos_.x, 0.01f);
+	ImGui::DragFloat3("cameraTranslate", &translation_.x, 0.01f);
 	ImGui::DragFloat3("cameraRotate_ ", &rotate_.x, 0.01f);
 
 	ImGui::End();
+}
+
+void Camera::RotateMove() {
+	
+	if (Novice::IsTriggerMouse(2)) {
+		Vec2 pos{};
+		Novice::GetMousePosition(&pos.x, &pos.y);
+
+	} else {
+		Novice::GetMousePosition(&mousePos_.x, &mousePos_.y);
+	}
 }
--- camera/Camera.h
+++ camera/Camera.h
@@ -1,4 +1,5 @@
-#pragma once
+﻿#pragma once
+#include "MyVector2.h"
 #include "MyMatrix4x4.h"
 #include "MyVector3.h"
 
@@ -9,18 +10,21 @@ class Camera{
 
 private:
 
-	Vec3f pos_;
+	Vec3f translation_;
 	Vec3f rotate_;
 
 	Matrix4x4 cameraMatrix_;
-
+	// ビュー行列
 	Matrix4x4 viewMatrix_;
-
+	// 射影行列
 	Matrix4x4 projectionMatrix_;
 
 	Matrix4x4 viewProjectMatrix_;
 
 	Matrix4x4 viewportMatrix_;
+
+	// マウスの座標
+	Vec2 mousePos_;
 
 public:
 
@@ -29,6 +33,12 @@ public:
 	void Init();
 	void Update();
 	void Draw();
+
+public:
+
+	void RotateMove();
+
+public:
 
 	Matrix4x4 GetViewMatrix() const { return viewMatrix_; }
 	Matrix4x4 GetProjectionMatrix() const { return projectionMatrix_; }
