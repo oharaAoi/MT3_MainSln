5f30761a282e9247058176fab1148b3e815a63d4
oharaAoi <k023g0108@g.neec.ac.jp>
2024-04-26 09:58:53 +0900

    マスターにcollisionの追加

--- main.cpp
+++ main.cpp
@@ -21,9 +21,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char preKeys[256] = {0};
 
 	std::unique_ptr<Camera> camera_ = std::make_unique<Camera>();
-	// sphereの初期化
-	Sphere sphere1 = { { -1.0f, 0.0f, -1.0f}, 0.5f , 0xffffffff };
-	Sphere sphere2 = { { 1.0f, 0.0f, 2.0f}, 0.5f , 0xffffffff };
+	
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -38,15 +36,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///------------------///
 
-		camera_->Update();
-
-		if (IsCollision(sphere1, sphere2)) {
-			sphere1.color = 0xff0000ff;
-		} else {
-			sphere1.color = 0xffffffff;
-		}
-
-
+		
 
 		///------------------///
 		/// ↑更新処理ここまで
@@ -60,19 +50,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 		DrawGrid(camera_->GetViewProjectMatrix(), camera_->GetViewportMatrix());
 
-		DrawSphere(sphere1, camera_->GetViewProjectMatrix(), camera_->GetViewportMatrix());
-		DrawSphere(sphere2, camera_->GetViewProjectMatrix(), camera_->GetViewportMatrix());
-
-		ImGui::Begin("Setting");
-
-		ImGui::Text("sphere1");
-		ImGui::DragFloat3("Sphere1.center", &sphere1.center.x, 0.1f);
-		ImGui::DragFloat("Sphere1.radius", &sphere1.radius, 0.1f);
-		ImGui::Text("sphere2");
-		ImGui::DragFloat3("Sphere2.center", &sphere2.center.x, 0.1f);
-		ImGui::DragFloat("Sphere2.radius", &sphere2.radius, 0.1f);
-
-		ImGui::End();
 
 		///------------------///
 		/// ↑描画処理ここまで
