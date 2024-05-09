f8af76f6dcb881b407ce781a0f61fbe8d185ad8f
oharaAoi <k023g0108@g.neec.ac.jp>
2024-05-09 12:14:27 +0900

    他の判定の追加

---
+++ LineCollider.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include "MyMath.h"
+#include "MyMatrix4x4.h"
+#include "DrawUtils.h"
+
+/// <summary>///
+/// 線分と平面の当たり判定
+/// </summary>///
+/// <param name="segment">線分</param>/// 
+/// <param name="plane">平面</param>///
+///  <returns>true: 当たっている false: 当たっていない</returns>
+bool IsCollision(const Segment& segment, const Plane& plane);
+
+/// <summary>///
+/// 直線と平面の当たり判定
+/// </summary>///
+/// <param name="segment">直線</param>/// 
+/// <param name="plane">平面</param>///
+///  <returns>true: 当たっている false: 当たっていない</returns>
+bool IsCollision(const Line& line, const Plane& plane);
+
+/// <summary>///
+/// 半直線と平面の当たり判定
+/// </summary>///
+/// <param name="segment">半直線</param>/// 
+/// <param name="plane">平面</param>///
+///  <returns>true: 当たっている false: 当たっていない</returns>
+bool IsCollision(const Ray& ray, const Plane& plane);
