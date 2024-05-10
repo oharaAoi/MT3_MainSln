4656c415238cb0836e83d17a5db71b8e26c77f52
oharaAoi <k023g0108@g.neec.ac.jp>
2024-05-10 13:05:24 +0900

    三角形の描画

--- renderUtils/DrawUtils.h
+++ renderUtils/DrawUtils.h
@@ -20,6 +20,10 @@ struct Plane {
 	float distance;	// 距離
 };
 
+struct Triangle {
+	Vec3f vertices[3];
+};
+
 /// <summary>
 /// グリット線を引く関数
 /// </summary>
@@ -43,6 +47,17 @@ void DrawSphere(const Sphere& sphere, const Matrix4x4& viewProjectionMatrix, con
 /// <param name="viewMatrix"></param>
 /// <param name="color"></param>
 void DrawPlane(const Plane& plane, const Matrix4x4& viewProjection, const Matrix4x4& viewportMatrix, const uint32_t& color);
+
+/// <summary>
+/// 三角形の描画
+/// </summary>
+/// <param name="triangle">頂点</param>
+/// <param name="viewProjection">正射影行列</param>
+/// <param name="viewportMatrix">ビューポート行列</param>
+/// <param name="color">色</param>
+/// <param name="isWier">塗りつぶすか</param>
+void DrawTriangle(const Triangle& triangle, const Matrix4x4& viewProjection, const Matrix4x4& viewportMatrix, const uint32_t& color, const bool& isWier);
+
 
 // 表示
 void VectorScreenPrintf(int x, int y, const Vec3f& vector, const char* label);
