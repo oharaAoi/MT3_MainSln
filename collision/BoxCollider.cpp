4a1218969f5daa198be3921c8f28ee99d56a35ff
oharaAoi <k023g0108@g.neec.ac.jp>
2024-05-17 10:32:36 +0900

    MT3_02_06_確認課題

--- collision/BoxCollider.cpp
+++ collision/BoxCollider.cpp
@@ -22,3 +22,22 @@ bool IsCollision(const AABB& aabb1, const AABB& aabb2) {
 	return true;
 
 }
+
+bool IsCollision(const AABB& aabb, const Sphere& sphere) {
+	// 最近傍点を求める
+	Vec3f closestPoint{
+		std::clamp(sphere.center.x, aabb.min.x, aabb.max.x),
+		std::clamp(sphere.center.y, aabb.min.y, aabb.max.y),
+		std::clamp(sphere.center.z, aabb.min.z, aabb.max.z)
+	};
+
+	// 最近傍点ト球の中心の距離を求める
+	float distance = Length(closestPoint - sphere.center);
+
+	// 距離が半径よりも小さければ衝突
+	if (distance <= sphere.radius) {
+		return true;
+	}
+
+	return false;
+}
--- collision/BoxCollider.h
+++ collision/BoxCollider.h
@@ -2,6 +2,7 @@
 #include "MyMath.h"
 #include "MyMatrix4x4.h"
 #include "DrawUtils.h"
+#include <algorithm>
 
 /// <summary>
 /// 当たり判定
@@ -11,3 +12,10 @@
 /// <returns>true: 当たっている false: 当たっていない</returns>
 bool IsCollision(const AABB& aabb1, const AABB& aabb2);
 
+/// <summary>
+/// 球との当たり判定
+/// </summary>
+/// <param name="aabb">箱</param>
+/// <param name="sphere">球</param>
+/// <returns>true: 当たっている false: 当たっていない</returns>
+bool IsCollision(const AABB& aabb, const Sphere& sphere);
