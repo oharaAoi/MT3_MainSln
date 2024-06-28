5f856e3d11b2a47daddc68f94c456ed00dfba71b
oharaAoi <k023g0108@g.neec.ac.jp>
2024-06-28 10:18:32 +0900

    main.cppの変更

--- main.cpp
+++ main.cpp
@@ -25,40 +25,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	std::unique_ptr<Camera> camera_ = std::make_unique<Camera>();
 
 	// ---------------------------------------------------------------
-	// ↓ OBB
+	// ↓ 
 	// ---------------------------------------------------------------
-	Vec3f rotate = { 0.0f, 0.0f, 0.0f };
-	OBB obb{
-		.center {-1.0f, 0.0f, 0.0f},
-		.orientations = {
-			{1.0f, 0.0f, 0.0f},
-			{0.0f, 1.0f, 0.0f},
-			{0.0f, 0.0f, 1.0f},
-		},
-		.size{0.5f, 0.5f, 0.5f}
-	};
-
-	obb.MakeOBBAxis(rotate);
-
-	uint32_t obbColor = WHITE;
+	
 
 	// ---------------------------------------------------------------
-	// ↓ OBB2
+	// ↓ 
 	// ---------------------------------------------------------------
-	Vec3f rotate2 = { -0.05f, -2.49f, 0.15f };
-	OBB obb2{
-		.center {0.9f, 0.66f, 0.78f},
-		.orientations = {
-			{1.0f, 0.0f, 0.0f},
-			{0.0f, 1.0f, 0.0f},
-			{0.0f, 0.0f, 1.0f},
-		},
-		.size{0.5f, 0.37f, 0.5f}
-	};
 
-	obb2.MakeOBBAxis(rotate2);
-
-	uint32_t obbColor2 = WHITE;
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
@@ -75,19 +49,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 		camera_->Update();
 
-		// ------------------------ OBBの回転要素を求める ------------------------ //
-
-		obb.MakeOBBAxis(rotate);
-		obb2.MakeOBBAxis(rotate2);
+		// ------------------------  ------------------------ //
 
 		// ------------------------ 当たり判定 ------------------------ //
-
-		if (IsCollision(obb, obb2)) {
-			obbColor2 = RED;
-		} else {
-			obbColor2 = WHITE;
-		}
-
 
 		///------------------///
 		/// ↑更新処理ここまで
@@ -101,27 +65,17 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 		DrawGrid(camera_->GetViewProjectMatrix(), camera_->GetViewportMatrix());
 
-		DrawOBB(obb, camera_->GetViewProjectMatrix(), camera_->GetViewportMatrix(), obbColor);
-		DrawOBB(obb2, camera_->GetViewProjectMatrix(), camera_->GetViewportMatrix(), obbColor2);
-
+		
 		ImGui::Begin("Set");
 
 		if (ImGui::TreeNode("OBB1")) {
-			ImGui::DragFloat3("obb.center", &obb.center.x, 0.1f, 0.1f);
-			ImGui::DragFloat3("rotate", &rotate.x, 0.1f);
-			ImGui::DragFloat3("size", &obb.size.x, 0.1f);
+		
 			ImGui::TreePop();
 		}
 
 		if (ImGui::TreeNode("OBB2")) {
-			ImGui::DragFloat3("obb.center", &obb2.center.x, 0.1f, 0.1f);
-			ImGui::DragFloat3("rotate", &rotate2.x, 0.1f);
-			ImGui::DragFloat3("size", &obb2.size.x, 0.1f);
-			ImGui::TreePop();
-		}
-
-		if (ImGui::TreeNode("Segment")) {
 			
+			ImGui::TreePop();
 		}
 
 		ImGui::End();
